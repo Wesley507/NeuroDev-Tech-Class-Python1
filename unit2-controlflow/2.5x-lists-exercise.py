cf0871b4ab30c15a9ddb069631a5804dfc605313
Wesley507 <Wesley@Neurodevmentoring.com>
2025-04-02 14:11:41 -0400

    python 4/2

--- unit2-controlflow/2.5x-lists-exercise.py
+++ unit2-controlflow/2.5x-lists-exercise.py
@@ -3,3 +3,15 @@
     # Prompts the user to enter 5 integers, one at a time, and appends them to my_list.
     # Prints the sum of all the integers in my_list to the user.
     # Prints the largest and smallest integers in my_list to the user using the max() and min() functions, respectively.
+
+my_list = []
+print("Please enter 5 integers:")
+for i in range(5):
+    number = int(input(f"Enter integer {i+1}: "))
+    my_list.append(number)
+total = sum(my_list)
+largest = max(my_list)
+smallest = min(my_list)
+print("The largest integer is ", largest)
+print("The smallest integer is ", smallest)
+print("The sum of all integers is", total)
